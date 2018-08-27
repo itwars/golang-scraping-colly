fa95a276c2ca618c4c0b2a68bebc498a2bd7a5ec
RABAH Vincent <ETP8013@zgie.ztech>
2018-08-27 17:03:27 +0200

    sports

---
+++ sports/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/itwars/golang-scraping-colly/sports/rugby"
+)
+
+func main() {
+	rugbyrama.Top14()
+}
---
+++ sports/rugby/rugbyrama.go
@@ -0,0 +1,39 @@
+// Package rugbyrama providing scrapping information from rugbyrama.fr
+package rugbyrama
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly"
+)
+
+type top14 struct {
+	Classement string `selector:".standing-table__cell--position"`
+	Club       string `selector:".standing-table__cell--team"`
+	NbPoints   string `selector:".standing-table__cell-value--main"`
+}
+
+// Top14 func récupère les informations qui sont sous la forme d'un json
+func Top14() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.rugbyrama.fr", "rugbyrama.fr"),
+	)
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Houston nous avons un problème avec Rugbyrama: ", err)
+	})
+	c.OnRequest(func(r *colly.Request) {
+		// fmt.Println("Je visite le site suivant : ", r.URL.String())
+	})
+	c.OnHTML("tr.standing-table__row", func(e *colly.HTMLElement) {
+		classement := &top14{}
+		e.Unmarshal(classement)
+		fmt.Println("Classement général : ", classement.Classement)
+		fmt.Println("Nom du club        : ", classement.Club)
+		fmt.Println("Nombre de points   : ", classement.NbPoints)
+	})
+	// En fin de scraping j'affiche le json avec toutes les informations récupérées
+	c.OnScraped(func(s *colly.Response) {
+	})
+	c.Visit("https://www.rugbyrama.fr/rugby/top-14/standing.shtml")
+}
