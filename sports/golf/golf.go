2905df283b24625c69b281ff63c621909901acee
RABAH Vincent <ETP8013@zgie.ztech>
2018-08-31 14:41:22 +0200

    ajout du golf

---
+++ sports/golf/golf.go
@@ -0,0 +1,43 @@
+// Package golf providing scrapping information from lefigaro
+package golf
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/gocolly/colly"
+)
+
+type leaderboard struct {
+	Rang     string `selector:".td_idalgo_standing_position"`
+	Nom      string `selector:".td_idalgo_standing_country > img" attr:"title"`
+	Pays     string `selector:".td_idalgo_standing_player"`
+	NbPoints string `selector:".td_idalgo_standing_points"`
+}
+
+// Leaderboard func récupère les informations qui sont sous la forme d'un json
+func Leaderboard() {
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.lefigaro.fr", "golf.lefigaro.fr"),
+	)
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Println("Houston nous avons un problème avec Le Figaro: ", err)
+	})
+	c.OnRequest(func(r *colly.Request) {
+		// fmt.Println("Je visite le site suivant : ", r.URL.String())
+		fmt.Println()
+		fmt.Println("Classement mondiale de golf")
+		fmt.Println()
+	})
+	c.OnHTML("tr.tr_idalgo_content_standing_line", func(e *colly.HTMLElement) {
+		classement := &leaderboard{}
+		e.Unmarshal(classement)
+		value, _ := strconv.ParseFloat(classement.NbPoints, 32)
+		fmt.Printf("%2s %-25s %-25s %3.2f point(s)\n", classement.Rang, classement.Nom, classement.Pays, value)
+	})
+	// En fin de scraping j'affiche le json avec toutes les informations récupérées
+	c.OnScraped(func(s *colly.Response) {
+	})
+	c.Visit("http://golf.lefigaro.fr/actualite/live/classement-mondial/1/classement")
+}
--- sports/main.go
+++ sports/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/itwars/golang-scraping-colly/sports/foot"
+	"github.com/itwars/golang-scraping-colly/sports/golf"
 	"github.com/itwars/golang-scraping-colly/sports/rugby"
 )
 
 func main() {
 	rugby.Top14()
 	foot.Ligue1()
+	golf.Leaderboard()
 }
